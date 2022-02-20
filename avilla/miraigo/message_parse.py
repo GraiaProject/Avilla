8b83fec82cbb8b5435cdedc4ea659099e1cd20cd
ProgramRipper <programripper@foxmail.com>
2022-02-20 12:53:36 +0800

    Update message_parse.py

--- avilla/miraigo/message_parse.py
+++ avilla/miraigo/message_parse.py
@@ -1,157 +1,98 @@
 from typing import TYPE_CHECKING, Any, Dict, Type
 
-from avilla.core.elements import Audio, Notice, NoticeAll, Text, Video
+from avilla.core.elements import Audio, Notice, NoticeAll, Text, Element
 from avilla.core.selectors import resource as resource_selector
 from avilla.core.utilles import Registrar
-from avilla.core.utilles.message import AbstractMessageParser
-from avilla.onebot.elements import *
+from avilla.onebot.message_parse import OnebotMessageParser
+from avilla.miraigo.elements import (
+    Reply,
+    Redbag,
+    Video,
+    XML,
+    Json,
+    Image,
+    FlashImage,
+    ShowImage,
+)
 
 if TYPE_CHECKING:
-    from avilla.onebot.protocol import OnebotProtocol
+    from avilla.miraigo.protocol import MiraigoProtocol
 
 registrar = Registrar()
 
 element_map: Dict[str, Type[Element]] = {
-    "text": Text,
     "reply": Reply,
-    "voice": Audio,
+    "redbag": Redbag,
     "video": Video,
-    "face": Face,
-    "rps": RPS,
-    "reply": Reply,
+    "xml": XML,
+    "json": Json,
 }
 
 
 @registrar.decorate("parsers")
-class OnebotMessageParser(AbstractMessageParser):
+class MiraigoMessageParser(OnebotMessageParser):
     def type_getter(self, token: Dict[str, Any]) -> Type[Element]:
         elem_type = token.get("type", "")
-        if elem_type == "at":
-            if token["data"]["qq"] == "all":
-                return NoticeAll
-            return Notice
-        elif elem_type == "image":
-            if token["data"].get("type") == "flash":
+        if elem_type == "image":
+            sub_type = token["data"].get("tyoe")
+            if sub_type == "flash":
                 return FlashImage
+            elif sub_type == "show":
+                return ShowImage
             return Image
         elif elem_type in element_map:
             return element_map[elem_type]
         else:
-            raise NotImplementedError(f"Unsupported message type: {elem_type}")
-
-    @staticmethod
-    @registrar.register(Text)
-    async def text(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Text:
-        return Text(data["data"]["text"])
-
-    @staticmethod
-    @registrar.register(Notice)
-    async def notice(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Notice:
-        return Notice(data["data"]["qq"])
-
-    @staticmethod
-    @registrar.register(NoticeAll)
-    async def notice_all(protocol: "OnebotProtocol", data: Dict[str, Any]) -> NoticeAll:
-        return NoticeAll()
+            return super().type_getter(token)
 
     @staticmethod
     @registrar.register(Image)
-    async def image(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Image:
-        return Image(resource_selector.image[data["data"]["url"]].provider[protocol.service.__class__])
-
-    @staticmethod
-    @registrar.register(FlashImage)
-    async def flash_image(protocol: "OnebotProtocol", data: Dict[str, Any]) -> FlashImage:
-        return FlashImage(resource_selector.image[data["data"]["url"]].provider[protocol.service.__class__])
-
-    @staticmethod
-    @registrar.register(Audio)
-    async def voice(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Audio:
-        return Audio(resource_selector.audio[data["data"]["url"]].provider[protocol.service.__class__])
-
-    @staticmethod
-    @registrar.register(Video)
-    async def video(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Video:
-        return Video(resource_selector.video[data["data"]["url"]].provider[protocol.service.__class__])
-
-    @staticmethod
-    @registrar.register(Face)
-    async def face(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Face:
-        return Face(data["data"]["id"])
-
-    @staticmethod
-    @registrar.register(RPS)
-    async def rps(protocol: "OnebotProtocol", data: Dict[str, Any]) -> RPS:
-        return RPS()
-
-    @staticmethod
-    @registrar.register(Dice)
-    async def dice(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Dice:
-        return Dice()
-
-    @staticmethod
-    @registrar.register(Shake)
-    async def shake(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Shake:
-        return Shake()
-
-    @staticmethod
-    @registrar.register(Poke)
-    async def poke(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Poke:
-        return Poke(data["data"]["type"], data["data"]["id"], data["data"]["name"])
-
-    @staticmethod
-    @registrar.register(Share)
-    async def share(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Share:
-        return Share(
-            data["data"]["url"],
-            data["data"]["title"],
-            data["data"]["content"],
-            data["data"]["image"],
+    async def image(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> Image:
+        return Image(
+            resource_selector.image[data["data"]["url"]].provider[protocol.service.__class__],
+            data["data"].get(["subType"]),
         )
 
     @staticmethod
-    @registrar.register(Contact)
-    async def contact(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Contact:
-        if data["data"]["type"] == "qq":
-            return Contact(entity_selector.friend[data["data"]["id"]])
-        return Contact(entity_selector.group[data["data"]["id"]])
+    @registrar.register(FlashImage)
+    async def flash_image(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> FlashImage:
+        return FlashImage(
+            resource_selector.image[data["data"]["url"]].provider[protocol.service.__class__],
+            data["data"].get(["subType"]),
+        )
 
     @staticmethod
-    @registrar.register(Location)
-    async def location(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Location:
-        return Location(
-            float(data["data"]["lat"]),
-            float(data["data"]["lon"]),
-            data["data"]["title"],
-            data["data"]["content"],
+    @registrar.register(ShowImage)
+    async def show_image(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> ShowImage:
+        return ShowImage(
+            resource_selector.image[data["data"]["url"]].provider[protocol.service.__class__],
+            data["data"].get(["subType"]),
         )
 
     @staticmethod
     @registrar.register(Reply)
-    async def reply(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Reply:
+    async def reply(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> Reply:
         return Reply(data["data"]["id"])
 
     @staticmethod
-    @registrar.register(Forward)
-    async def forward(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Forward:
-        return Forward(data["data"]["id"])
+    @registrar.register(Redbag)
+    async def redbag(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> Redbag:
+        return Redbag(data["data"]["title"])
 
     @staticmethod
-    @registrar.register(Node)
-    async def node(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Node:
-        return Node(
-            None,
-            data["data"]["user_id"],
-            data["data"]["nickname"],
-            await protocol.parse_message(data["data"]["message"]),
+    @registrar.register(Video)
+    async def video(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> Video:
+        return Video(
+            resource_selector.video[data["data"]["url"]].provider[protocol.service.__class__],
+            resource_selector.image[data["data"]["cover"]].provider[protocol.service.__class__],
         )
 
     @staticmethod
     @registrar.register(XML)
-    async def xml(protocol: "OnebotProtocol", data: Dict[str, Any]) -> XML:
+    async def xml(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> XML:
         return XML(data["data"]["data"])
 
     @staticmethod
     @registrar.register(Json)
-    async def json(protocol: "OnebotProtocol", data: Dict[str, Any]) -> Json:
+    async def json(protocol: "MiraigoProtocol", data: Dict[str, Any]) -> Json:
         return Json(data["data"]["data"])
