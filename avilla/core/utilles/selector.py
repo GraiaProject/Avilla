d9ae2abef8b24783dd889b174ce74e0c3db442c8
Bryan不可思议 <programripper@foxmail.com>
2022-07-12 10:39:30 +0800

    refactor(core): refactor Selector (#54)
    * refactor(core): refactor Selector
    
    * fix(core): resolve change requests

--- avilla/core/utilles/selector.py
+++ avilla/core/utilles/selector.py
@@ -1,192 +1,187 @@
 from __future__ import annotations
 
 from collections import ChainMap
-from typing import (
-    Any,
-    Callable,
-    Generic,
-    Literal,
-    Protocol,
-    TypeGuard,
-    TypeVar,
-    Union,
-    runtime_checkable,
-)
+from itertools import filterfalse
+from typing import Any, Callable, Literal, Protocol, runtime_checkable
 
-from typing_extensions import LiteralString
+from typing_extensions import Self
 
 MatchRule = Literal["exact", "exist", "any", "fragment"]
-Pattern = Union[str, Callable[[str], bool]]
-
-P = TypeVar("P", bound=str)
-TLiteral = TypeVar("TLiteral", bound=LiteralString)
+Pattern = str | Callable[[str], bool]
 
 
-# TODO: 大概就是默认只接受 str 而不是 callable... 然后提供一个 DynamicSelector 实现.
-
-
-class Selector(Generic[P]):
+class Selector:
     mode: MatchRule = "exact"
+    pattern: dict[str, str]
+    path_excludes: frozenset[str]
+    referent: Any = None
 
-    pattern: dict[str, Pattern]
-    path_excludes: tuple[str, ...]
-
-    def __init__(self, *, mode: MatchRule = "exact", path_excludes: tuple[str, ...] | None = None):
+    def __init__(self, *, mode: MatchRule = "exact", path_excludes: frozenset[str] = frozenset()) -> None:
         self.mode = mode
-        self.path_excludes = path_excludes or ()
+        self.path_excludes = path_excludes
         self.pattern = {}
 
-    def __getattr__(self, name: str):
-        def wrapper(content: Pattern | Literal["*"]):
-            if content == "*":
-                content = lambda _: True
+    def __getattr__(self, name: str) -> Callable[[str], Self]:
+        def wrapper(content: str) -> Self:
             self.pattern[name] = content
             return self
 
         return wrapper
 
-    def contains(self, key: str) -> bool:
+    def __contains__(self, key: str) -> bool:
         return key in self.pattern
 
-    __contains__ = contains
-
-    def __getitem__(self, key: str) -> Pattern:
+    def __getitem__(self, key: str) -> str:
         return self.pattern[key]
 
     def __repr__(self) -> str:
-        return f"Selector(mode={self.mode})" + (
-            "." + ".".join([f"{k}({v})" for k, v in self.pattern.items()]) if self.pattern else ""
-        )
-
-    @property
-    def constant(self) -> bool:
-        return all(not callable(v) for v in self.pattern.values())
+        return f"Selector(mode={self.mode}).{'.'.join(f'{k}({v})' for k, v in self.pattern.items())}"
 
     @property
     def empty(self) -> bool:
         return not self.pattern
 
     @property
-    def path(self) -> P | str:
-        return ".".join(k for k in self.pattern.keys() if k not in self.path_excludes)
+    def path(self) -> str:
+        return ".".join(filterfalse(self.path_excludes.__contains__, self.pattern))
 
     @classmethod
-    def exist(cls):
+    def exist(cls) -> Self:
         return cls(mode="exist")
 
     @classmethod
-    def any(cls):
+    def any(cls) -> Self:
         return cls(mode="any")
 
     @classmethod
-    def fragment(cls, *path_excludes: str) -> Selector:
-        return cls(mode="fragment", path_excludes=path_excludes)
+    def fragment(cls, *path_excludes: str) -> Self:
+        return cls(mode="fragment", path_excludes=frozenset(path_excludes))
 
-    @classmethod
-    def way(cls, path: TLiteral) -> Selector[TLiteral]:
-        instance = cls()
-        instance.pattern = {i: lambda _: True for i in path.split(".")}
-        return instance  # type: ignore
+    def match(self, other: Selector) -> bool:
+        try:
+            match = {
+                "exact": self._match_exact,
+                "exist": self._match_exist,
+                "any": self._match_any,
+                "fragment": self._match_fragment,
+            }[self.mode]
+        except KeyError:
+            raise ValueError(f"Unknown match rule: {self.mode}") from None
 
-    def assert_constant(self: Selector) -> TypeGuard[ConstantSelector]:
-        return self.constant
+        return match(other)
 
-    def match(self, another: Selector) -> bool:
-        # sourcery skip: low-code-quality
-        if self.mode == "exact":
-            if self.constant:
-                return another.constant and self.path == another.path and self.pattern == another.pattern
-            if not another.constant:
-                raise TypeError("Can't match dynamic selector with another dynamic selector")
-            for k, v1 in self.pattern.items():
-                v2 = another.pattern[k]
-                if v1 != v2:
-                    return False
-            return True
-        elif self.mode == "exist":
-            subset = set(self.pattern.keys()).issubset(another.pattern.keys())
-            if not subset:
-                return False
-            if not another.constant and any(
-                callable(v) for k, v in another.pattern.items() if k in self.pattern
-            ):
-                raise TypeError("Can't partially match dynamic selector with another dynamic selector")
-            for k, v1 in self.pattern.items():
-                if k in another.pattern:
-                    v2 = another.pattern[k]
-                    if callable(v1):
-                        if callable(v2):
-                            raise TypeError(
-                                "Can't partially match dynamic selector with another dynamic selector"
-                            )
-                        if not v1(v2):
-                            return False
-                    elif v1 != v2:
-                        return False
-            return True
-        elif self.mode == "fragment":
-            if self.empty:
-                return True
-            fragment = list(self.pattern.keys())
-            full = list(another.pattern.keys())
-            try:
-                start = full.index(fragment[0])
-            except IndexError:
-                return False
-            sub = full[start : start + len(fragment)]
-            if len(sub) != len(fragment):
-                return False
-            for k1, k2 in zip(fragment, sub):
-                if k1 != k2:
-                    return False
-                v1 = self.pattern[k1]
-                v2 = another.pattern[k2]
-                if callable(v1):
-                    if callable(v2):
-                        raise TypeError(
-                            "Can't partially match dynamic selector with another dynamic selector"
-                        )
-                    if not v1(v2):
-                        return False
-                elif v1 != v2:
-                    return False
-            return True
-        elif self.mode == "any":
-            return True
-        else:
-            raise ValueError(f"Unknown match rule: {self.mode}")
+    def _match_exact(self, other: Selector) -> bool:
+        return type(other) is Selector and self.path == self.path and self.pattern == other.pattern
 
-    def mix(self, path: str, **env: Pattern) -> Selector:
-        env = self.pattern.copy() | env
-        instance = super().__new__(self.__class__)
-        instance.__init__(mode=self.mode, path_excludes=self.path_excludes)
-        if not set(env).issuperset(path.split(".")):
-            raise ValueError(f"given information cannot mix with {path}")
-        instance.pattern = {each: env[each] for each in path.split(".")}
+    def _match_exist(self, other: Selector) -> bool:
+        return set(self.pattern.items()).issubset(other.pattern.items())
+
+    def _match_fragment(self, other: Selector) -> bool:
+        fragment = list(self.pattern.items())
+        full = list(other.pattern.items())
+
+        try:
+            start = full.index(fragment[0])
+        except IndexError:
+            return True
+        except ValueError:
+            return False
+
+        return full[start : start + len(fragment)] == fragment
+
+    def _match_any(self, other: Selector) -> bool:
+        return True
+
+    def mix(self, path: str, **pattern: str) -> Self:
+        pattern = self.pattern | pattern
+        instance = self.__class__(mode=self.mode, path_excludes=self.path_excludes)
+
+        try:
+            instance.pattern = {each: pattern[each] for each in path.split(".")}
+        except KeyError:
+            raise ValueError(f"given information cannot mix with {path}") from None
+
         return instance
 
-    def mixin(self, path: str, *selectors: Selector) -> Selector:
+    def mixin(self, path: str, *selectors: Self) -> Self:
         return self.mix(path, **ChainMap(*(x.pattern for x in selectors)))
 
-    def copy(self) -> Selector:
-        instance = super().__new__(self.__class__)
-        instance.mode = self.mode
+    def copy(self) -> Self:
+        instance = self.__class__(mode=self.mode, path_excludes=self.path_excludes)
         instance.pattern = self.pattern.copy()
         return instance
 
-    referent: Any = None
-
-    def set_referent(self, referent: Any):
+    def set_referent(self, referent: Any) -> Self:
         self.referent = referent
         return self
+
+
+class DynamicSelector(Selector):
+    pattern: dict[str, Pattern]
+
+    def __getattr__(self, name: str, /):
+        def wrapper(content: Pattern | Literal["*"]):
+            if content == "*":
+                content = lambda _: True
+
+            self.pattern[name] = content
+            return self
+
+        return wrapper
+
+    __getitem__: Callable[[str], Pattern]
+
+    @classmethod
+    def way(cls, path: str) -> Selector:
+        instance = cls()
+        instance.pattern = {i: lambda _: True for i in path.split(".")}
+        return instance
+
+    def _match_exact(self, other: Selector) -> bool:
+        if isinstance(other, DynamicSelector):
+            raise TypeError("Can't match dynamic selector with another dynamic selector")
+        return set(other.pattern.items()).issubset(self.pattern.items())
+
+    def _match_exist(self, other: Selector) -> bool:
+        for a, b in ((self.pattern[path], other.pattern[path]) for path in self.pattern):
+            if callable(a):
+                if callable(b):
+                    raise TypeError("Can't partially match dynamic selector with another dynamic selector")
+                elif not a(b):
+                    return False
+            elif a != b:
+                return False
+
+        return True
+
+    def _match_fragment(self, other: Selector) -> bool:
+        fragment = list(self.pattern)
+        full = list(other.pattern)
+
+        try:
+            start = full.index(fragment[0])
+        except IndexError:
+            return True
+        except ValueError:
+            return False
+
+        full = full[start : start + len(fragment)]
+        if fragment != full:
+            return False
+
+        for a, b in ((self.pattern[path], other.pattern[path]) for path in fragment):
+            if callable(a):
+                if callable(b):
+                    raise TypeError("Can't partially match dynamic selector with another dynamic selector")
+                elif not a(b):
+                    return False
+            elif a != b:
+                return False
+
+        return True
 
 
 @runtime_checkable
 class Summarizable(Protocol):
     def to_selector(self) -> Selector:
         ...
-
-
-# 似乎没啥用...
-class ConstantSelector(Selector):
-    pattern: dict[str, str]
