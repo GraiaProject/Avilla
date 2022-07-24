489e9ad88c74d9a1f53d2898a9da971bf81e4858
Elaina <31543961+GreyElaina@users.noreply.github.com>
2022-07-24 13:13:14 +0000

    complete lifecycle event

--- avilla/core/event/lifecycle.py
+++ avilla/core/event/lifecycle.py
@@ -1,8 +1,8 @@
-"""Ariadne, Adapter 生命周期相关事件"""
-from typing import TYPE_CHECKING
 from dataclasses import dataclass
-from graia.broadcast.entities.event import Dispatchable
+from typing import TYPE_CHECKING
+
 from graia.broadcast.entities.dispatcher import BaseDispatcher
+from graia.broadcast.entities.event import Dispatchable
 from graia.broadcast.interfaces.dispatcher import DispatcherInterface
 
 if TYPE_CHECKING:
@@ -13,18 +13,15 @@ class AvillaLifecycleEvent(Dispatchable):
     """指示有关应用 (Avilla) 的事件."""
 
 
-# CleanUp
-class AvillaStopping(AvillaLifecycleEvent):
+class ApplicationClosing(AvillaLifecycleEvent):
     """指示 Avilla 正在关闭."""
 
 
-# Finished
-class AvillaFinish(AvillaLifecycleEvent):
+class ApplicationClosed(AvillaLifecycleEvent):
     """指示 Avilla 已经关闭."""
 
 
-# Blocking
-class AvillaReady(AvillaLifecycleEvent):
+class ApplicationReady(AvillaLifecycleEvent):
     """指示 Avilla 已准备完毕."""
 
 
