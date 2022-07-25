4ae8ea8c9eb3de0e9c0f661d1b3760691597d9ac
Elaina <31543961+GreyElaina@users.noreply.github.com>
2022-07-25 07:40:35 +0000

    stash changes

--- avilla/elizabeth/protocol.py
+++ avilla/elizabeth/protocol.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import ClassVar
 
-from avilla.core import Avilla
+from avilla.core.application import Avilla
 from avilla.core.platform import Abstract, Land, Platform
 from avilla.core.protocol import BaseProtocol
+from avilla.core.querier import ProtocolAbstractQueryHandler
 from avilla.core.resource import PlatformResourceProvider
 from avilla.core.utilles.metadata_source import ProtocolMetadataSource
 from avilla.core.utilles.selector import Selector
@@ -43,13 +44,17 @@ class ElizabethProtocol(BaseProtocol):
 
     platform_resource_providers: ClassVar[dict[Selector, type[PlatformResourceProvider]]] = {}
     protocol_metadata_providers: ClassVar[list[type[ProtocolMetadataSource]]] = []
+    protocol_query_handlers: ClassVar[list[type[ProtocolAbstractQueryHandler]]] = [
+
+    ]
 
     # 鉴于你 mah 乃至 mirai 还没支持频道, 这里就直接.
     completion_rules: ClassVar[dict[str, list[str]]] = {
         "group": ["land.group"],
         "friend": ["land.friend"],
         "member": ["land.group.member"],
-        "contact": ["land.group.member"]  # Notice.target
+        "contact": ["land.group.member"],  # Notice.target
+        "group.member": ["land.group.member"],
         # TODO
     }
 
