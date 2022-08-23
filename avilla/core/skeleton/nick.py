981cf5ea5e1c7a0af37d5b42b5a9b13cf14c8743
Elaina <31543961+GreyElaina@users.noreply.github.com>
2022-08-23 10:41:51 +0000

    feat(core): NickTrait, with experimently badge support

---
+++ avilla/core/skeleton/nick.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from avilla.core.traitof import Fn, OrientedFn, Trait
+from avilla.core.utilles.selector import Selector
+
+
+class NickTrait(Trait):
+    @OrientedFn
+    async def set_name(self, name: str) -> None:
+        ...
+
+    @OrientedFn
+    async def set_nickname(self, nickname: str) -> None:
+        ...
+
+    @OrientedFn
+    async def unset_nickname(self) -> None:
+        ...
+
+    # TODO: recheck badge operators
+
+    @Fn
+    async def set_badge(self, target: Selector, badge: str) -> None:
+        ...
+
+    @Fn
+    async def unset_badge(self, target: Selector) -> None:
+        ...
