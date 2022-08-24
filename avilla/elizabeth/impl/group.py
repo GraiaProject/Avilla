48df698073608e0b47902d94b9ee2d05fe14b5bf
Elaina <31543961+GreyElaina@users.noreply.github.com>
2022-08-24 13:38:39 +0000

    feat(elizabeth): attempt to apply traits

--- avilla/elizabeth/impl/group.py
+++ avilla/elizabeth/impl/group.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
+from datetime import timedelta
 
 from typing import TYPE_CHECKING
 
 from avilla.core.message import Message
 from avilla.core.skeleton.message import MessageTrait
+from avilla.core.skeleton.privilege import Mute
+from avilla.core.cell.cells import Nick, Privilege, Summary
+from avilla.core.skeleton.scene import SceneTrait
+from avilla.core.skeleton.summary import SummaryTrait
 from avilla.core.traitof.context import prefix, raise_for_no_namespace, scope
-from avilla.core.traitof.recorder import default_target, impl
+from avilla.core.traitof.recorder import default_target, impl, pull
 from avilla.core.utilles.selector import Selector
 
 if TYPE_CHECKING:
@@ -47,4 +52,128 @@ with scope("group"), prefix("group"):
                 "messageId": int(message.pattern["message"]),
                 "target": int(message.pattern["group"]),
             },
+        )
+
+    @impl(Mute.mute)
+    async def mute_member(rs: Relationship, target: Selector, duration: timedelta):
+        privilege_info = await rs.pull(Privilege, target)
+        if not privilege_info.effective:
+            raise PermissionError() # TODO: error message, including Summary info
+        time = max(0, min(int(duration.total_seconds()), 2592000))  # Fix time parameter
+        if not time:
+            return
+        await rs.account.call(
+            "mute",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+                "memberId": int(target.pattern["member"]),
+                "time": time
+            },
+        )
+    
+    @impl(Mute.unmute)
+    async def unmute_member(rs: Relationship, target: Selector):
+        privilege_info = await rs.pull(Privilege, target)
+        if not privilege_info.effective:
+            raise PermissionError() # TODO: error message, including Summary info
+        await rs.account.call(
+            "unmute",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+                "memberId": int(target.pattern["member"]),
+            },
+        )
+
+    @impl(Mute.mute_all)
+    async def group_mute_all(rs: Relationship, target: Selector):
+        await rs.account.call(
+            "muteAll",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+            },
+        )
+
+    @impl(Mute.unmute_all)
+    async def group_unmute_all(rs: Relationship, target: Selector):
+        await rs.account.call(
+            "unmuteAll",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+            },
+        )
+
+    @impl(SceneTrait.leave).pin("group")
+    async def leave(rs: Relationship, target: Selector):
+        await rs.account.call(
+            "quit",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"])
+            }
+        )
+
+    
+    @impl(SceneTrait.remove_member)
+    async def remove_member(rs: Relationship, target: Selector, reason: str | None = None):
+        privilege_info = await rs.pull(Privilege, target)
+        if not privilege_info.effective:
+            raise PermissionError() # TODO: error message, including Summary info
+        await rs.account.call(
+            "kick",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+                "memberId": int(target.pattern["member"])
+            }
+        )
+
+    @pull(Summary).of("group")
+    async def get_summary(rs: Relationship, target: Selector | None) -> Summary:
+        assert target is not None
+        result = await rs.account.call(
+            "groupConfig",
+            {
+                "__method__": "get",
+                "target": int(target.pattern["group"])
+            },
+        )
+        return Summary(
+            describe=Summary,
+            name=result['name'],
+            description=None
+        )
+
+    @impl(SummaryTrait.set_name).pin("group")
+    async def group_set_name(rs: Relationship, target: Selector, name: str):
+        await rs.account.call(
+            "groupConfig",
+            {
+                "__method__": "post",
+                "target": int(target.pattern["group"]),
+                "config": {
+                    "name": name
+                }
+            },
+        )
+
+    @pull(Nick).of("group.member")
+    async def get_member_nick(rs: Relationship, target: Selector | None) -> Nick:
+        assert target is not None
+        result = await rs.account.call(
+            "memberInfo",
+            {
+                "__method__": "get",
+                "target": int(target.pattern["group"]),
+                "memberId": int(target.pattern["member"])
+            },
+        )
+        return Nick(
+            Nick,
+            result["memberName"],
+            result["memberName"],
+            result["specialTitle"]
         )
