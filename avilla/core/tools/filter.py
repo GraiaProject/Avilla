d4df79ee3a1c426edc32f7a0ef34f2f5f286fa5c
GreyElaina <GreyElaina@outlook.com>
2023-01-19 22:01:31 +0800

    refactor(core/tools/filter): improve filter ux

--- avilla/core/tools/filter.py
+++ avilla/core/tools/filter.py
@@ -4,25 +4,39 @@ from collections.abc import Awaitable, Callable, Container, Iterable
 from functools import partial
 from inspect import isawaitable
 from operator import contains, eq, is_, is_not, ne
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, Protocol, TypeVar
 
 from graia.broadcast import BaseDispatcher, DispatcherInterface, ExecutionStop
 from typing_extensions import ParamSpec, Self
 
 from avilla.core.account import AbstractAccount
 from avilla.core.context import Context
+from avilla.core.event import MetadataModified
+from avilla.core.metadata import MetadataFieldReference
 from avilla.core.selector import Selectable, Selector
 from avilla.core.utilles import classproperty
 
-T = TypeVar("T")
+T = TypeVar("T", covariant=True)
 R = TypeVar("R")
 P = ParamSpec("P")
+
+
+class _ClientSpecified(Protocol[R]):
+    client: R
+
+
+class _EndpointSpecified(Protocol[R]):
+    endpoint: R
+
+
+class _SceneSpecified(Protocol[R]):
+    scene: R
 
 
 class Filter(BaseDispatcher, Generic[T]):
     middlewares: list[Callable[[Any], Awaitable[Any] | Any]]
 
-    def fetch(self, annotation: type[R]) -> Filter[R]:
+    def dispatch(self, annotation: type[R]) -> Filter[R]:
         @self.middlewares.append
         async def _(_) -> R:
             interface = DispatcherInterface.ctx.get()
@@ -36,9 +50,9 @@ class Filter(BaseDispatcher, Generic[T]):
         self.middlewares.append(func)
         return self  # type: ignore
 
-    def assert_true(self, func: Callable[[T], Any]) -> Self:
+    def assert_true(self: Filter[R], func: Callable[[R], Any]):
         @self.step
-        def _(result: T) -> T:
+        def _(result: R) -> R:
             if func(result):
                 return result
             raise ExecutionStop
@@ -48,34 +62,34 @@ class Filter(BaseDispatcher, Generic[T]):
     def assert_false(self, func: Callable[[T], Any]) -> Self:
         return self.assert_true(lambda result: not func(result))
 
-    def assert_equal(self, value: R) -> R:
-        return self.assert_true(partial(eq, value))  # type: ignore
+    def assert_equal(self, value: Any) -> Self:
+        return self.assert_true(partial(eq, value))
 
     def assert_not_equal(self, value: Any) -> Self:
         return self.assert_true(partial(ne, value))
 
-    def assert_is(self, value: R) -> Filter[R]:
-        return self.assert_true(partial(is_, value))  # type: ignore
+    def is_(self, value: Any):
+        return self.assert_true(partial(is_, value))
 
-    def assert_is_not(self, value: Any) -> Self:
+    def is_not(self, value: Any) -> Self:
         return self.assert_true(partial(is_not, value))
 
-    def assert_is_none(self) -> Filter[None]:
-        return self.assert_is(None)
+    def is_none(self):
+        return self.is_(None)
 
-    def assert_is_not_none(self) -> Self:  # 实际上类型是 Filter[T - None]
-        return self.assert_is_not(None)
+    def is_not_none(self) -> Self:  # 实际上类型是 Filter[T - None]
+        return self.is_not(None)
 
-    def assert_in(self, container: Container[R]) -> Filter[R]:
+    def contains(self, container: Container[R]) -> Filter[R]:
         return self.assert_true(partial(contains, container))  # type: ignore
 
-    def assert_not_in(self, container: Container) -> Self:
+    def not_contains(self, container: Container) -> Self:
         return self.assert_true(lambda result: result not in container)
 
-    def assert_is_instance(self, cls: type[R]) -> Filter[R]:
+    def instanceof(self, cls: type[R]) -> Filter[R]:
         return self.assert_true(lambda result: isinstance(result, cls))  # type: ignore
 
-    def assert_not_is_instance(self, cls: type) -> Self:
+    def not_instanceof(self, cls: type) -> Self:
         return self.assert_true(lambda result: not isinstance(result, cls))
 
     def any(self, funcs: Iterable[Callable[[T], Any]]) -> Self:
@@ -110,23 +124,36 @@ class Filter(BaseDispatcher, Generic[T]):
     @classproperty
     @classmethod
     def ctx(cls) -> Filter[Context]:
-        return cls().fetch(Context)
+        return cls().dispatch(Context)
 
     @property
-    def client(self: Filter[Context]) -> Filter[Selectable]:
-        return self.ctx.step(lambda ctx: ctx.client)
+    def client(self: Filter[_ClientSpecified[R]]) -> Filter[R]:
+        return self.step(lambda x: x.client)
 
     @property
-    def scene(self: Filter[Context]) -> Filter[Selector]:
-        return self.ctx.step(lambda ctx: ctx.scene)
+    def scene(self: Filter[_SceneSpecified[R]]) -> Filter[R]:
+        return self.step(lambda x: x.scene)
 
     @property
-    def account(self: Filter[Context]) -> Filter[AbstractAccount]:
-        return self.ctx.step(lambda ctx: ctx.account)
+    def endpoint(self: Filter[_EndpointSpecified[R]]) -> Filter[R]:
+        return self.step(lambda x: x.endpoint)
 
-    @property
-    def mediums_first(self: Filter[Context]) -> Filter[Selector | None]:
-        return self.ctx.step(lambda ctx: ctx.mediums[0].selector if ctx.mediums else None)
+    @classmethod
+    def account(cls) -> Filter[AbstractAccount]:
+        return cls.ctx.step(lambda ctx: ctx.account)
+
+    def medium(self: Filter[Context], index: int = 0) -> Filter[Selector | None]:
+        return self.ctx.step(lambda ctx: ctx.mediums[index].selector if ctx.mediums else None)
+
+    @classmethod
+    def mod(cls, *fields: MetadataFieldReference) -> Filter[MetadataModified]:
+        return (
+            cls()
+            .dispatch(MetadataModified)
+            .assert_true(
+                lambda x: {k.field for i in x.modifies for j in i.effects.values() for k in j}.issuperset(fields)
+            )
+        )
 
     async def beforeExecution(self, interface: DispatcherInterface):
         result: Awaitable[Any] | Any = interface  # type: ignore
