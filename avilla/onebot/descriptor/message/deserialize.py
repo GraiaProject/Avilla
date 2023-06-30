bab31cc5a354f2397cba0fb03df3713f56ba71cb
GreyElaina <GreyElaina@outlook.com>
2023-06-30 23:11:09 +0800

    feat(onebot): message (de)serialize ryanvk descriptor

---
+++ avilla/onebot/descriptor/message/deserialize.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Awaitable, Callable, TypeVar
+
+from avilla.core._vendor.dataclasses import dataclass
+
+if TYPE_CHECKING:
+    from avilla.core.ryanvk.collector.protocol import (
+        ProtocolBasedPerformTemplate,
+        ProtocolCollector,
+    )
+    from graia.amnesia.message.element import Element
+
+PBPT = TypeVar("PBPT", bound="ProtocolBasedPerformTemplate", contravariant=True)
+
+@dataclass
+class MessageDeserializeSign:
+    element_type: str
+
+
+class OneBot11MessageDeserialize:
+    @classmethod
+    def collect(cls, collector: ProtocolCollector, element_type: str):
+        def receiver(entity: Callable[[PBPT, dict], Awaitable[Element]]):
+            collector.artifacts[MessageDeserializeSign(element_type)] = (collector, entity)
+            return entity
+
+        return receiver
---
+++ avilla/onebot/descriptor/message/serialize.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Callable, TypeVar, Generic, Awaitable
+
+from avilla.core._vendor.dataclasses import dataclass
+
+if TYPE_CHECKING:
+    from avilla.core.ryanvk.collector.protocol import (
+        ProtocolBasedPerformTemplate,
+        ProtocolCollector,
+    )
+    from graia.amnesia.message.element import Element
+
+PBPT = TypeVar("PBPT", bound="ProtocolBasedPerformTemplate", contravariant=True)
+E = TypeVar("E", bound="Element")
+
+@dataclass
+class MessageSerializeSign(Generic[E]):
+    element_type: type[Element]
+
+
+class OneBot11MessageSerialize:
+    @staticmethod
+    def collect(collector: ProtocolCollector, element_type: type[E]):
+        def receiver(entity: Callable[[PBPT, E], Awaitable[dict]]):
+            collector.artifacts[MessageSerializeSign(element_type)] = (collector, entity)
+            return entity
+
+        return receiver
