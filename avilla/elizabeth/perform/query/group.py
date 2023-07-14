46b8b79258ea67de03d2b311c3c5017aa5c6b7cb
GreyElaina <GreyElaina@outlook.com>
2023-07-14 21:11:33 +0800

    feat(elizabeth): query for group member

--- avilla/elizabeth/perform/query/group.py
+++ avilla/elizabeth/perform/query/group.py
@@ -15,8 +15,15 @@ class ElizabethGroupQueryPerform((m := AccountCollector["ElizabethProtocol", "El
     m.post_applying = True
 
     @CoreCapability.query.collect(m, "land.group")
-    async def query_group(self, predicate: Callable[[str, str], bool] | str, previous: Selector | None = None):
+    async def query_group(self, predicate: Callable[[str, str], bool] | str, previous: None):
         result = await self.account.connection.call("fetch", "groupList", {})
         result = cast(list, result)
         for i in result:
             yield Selector().land(self.account.route["land"]).group(str(i["id"]))
+
+    @CoreCapability.query.collect(m, "member", "land.group")
+    async def query_group_members(self, predicate: Callable[[str, str], bool] | str, previous: Selector):
+        result = await self.account.connection.call("fetch", "memberList", {"target": int(previous["group"])})
+        result = cast(list, result)
+        for i in result:
+            yield previous.member(str(i["id"]))
