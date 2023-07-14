8789967701cf813302d89b0bae27e9f0e5e2215a
GreyElaina <GreyElaina@outlook.com>
2023-07-14 11:01:50 +0800

    chore(onebot/v11): remove defined get_context

--- avilla/onebot/v11/account.py
+++ avilla/onebot/v11/account.py
@@ -4,7 +4,6 @@ from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
 from avilla.core.account import AccountStatus, BaseAccount
-from avilla.core.context import Context
 from avilla.core.selector import Selector
 from avilla.standard.core.account import AccountAvailable, AccountUnavailable
 from launart.utilles import any_completed
@@ -35,9 +34,6 @@ class OneBot11Account(BaseAccount):
         if prev != (curr := self.available):
             avilla = self.protocol.avilla
             avilla.broadcast.postEvent((AccountAvailable if curr else AccountUnavailable)(avilla, self))
-
-    async def get_context(self, target: Selector, *, via: Selector | None = None) -> Context:
-        ...
 
     async def call(self, endpoint: str, params: dict):
         coros = set()
