e10cf16bf71cfb070af47da8c999fae92294f0c0
GreyElaina <GreyElaina@outlook.com>
2023-08-06 22:11:38 +0800

    deps: use graia-amnesia 0.8.1

--- graia/amnesia/message/__init__.py
+++
@@ -1,8 +0,0 @@
-from .chain import MessageChain as MessageChain
-from .element import Element as Element
-from .element import Text as Text
-from .element import Unknown as Unknown
-from .formatter import Formatter as Formatter
-
-Element._chain_class = MessageChain
-MessageChain._text_class = Text
