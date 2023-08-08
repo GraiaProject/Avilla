584ddbab1d9dba7c0d74db65cc037254cc2e6139
RF-Tar-Railt <3165388245@qq.com>
2023-08-09 00:28:16 +0800

    feat(core): command

--- test-alconna.py
+++ test-alconna.py
@@ -1,4 +1,7 @@
 from arclet.alconna import Alconna, Args, Option, Subcommand, count
+from avilla.console.protocol import ConsoleProtocol
+from avilla.core import Avilla, Context
+from avilla.core.builtins.command import AvillaCommands
 
 alc = Alconna(
     "pip",
@@ -10,3 +13,18 @@ alc = Alconna(
     ),
     Option("-v|--version", action=count),
 )
+
+
+avilla = Avilla()
+avilla.apply_protocols(ConsoleProtocol())
+cmd = AvillaCommands()
+
+@cmd.on(Alconna("ping"))
+async def test(ctx: Context):
+    await ctx.scene.send_message("pong")
+
+@cmd.on(alc)
+async def test(package: str, ctx: Context):
+    await ctx.scene.send_message(f"installing {package}")
+
+avilla.launch()
