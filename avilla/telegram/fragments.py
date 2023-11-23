5a04dcee9bb36aa499203847b34234eddae40b81
nullqwertyuiop <null@member.fsf.org>
2023-11-23 22:47:34 +0800

    feat(telegram): placeholder for fragments
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- avilla/telegram/fragments.py
+++ avilla/telegram/fragments.py
@@ -1,24 +1,37 @@
 from pathlib import Path
 from typing import IO
 
-from telegram import InputFile, Message, PhotoSize, Update
+from telegram import (
+    InputFile,
+    InputMediaAudio,
+    InputMediaDocument,
+    InputMediaPhoto,
+    InputMediaVideo,
+    Message,
+    PhotoSize,
+    Update,
+)
 from telegram.constants import MessageType
 from telegram.ext import ExtBot
 from typing_extensions import Self
+
+from avilla.standard.telegram.elements import Contact
+
+_MISSING = object()  # Used for elements that are not in MessageType but in API
 
 
 class MessageFragment:
     type: MessageType
     update: Update | None
 
-    def __init__(self, _type: MessageType, update: Update | None = None):
-        self.type = _type
+    def __init__(self, msg_type: MessageType | _MISSING, update: Update | None = None):
+        self.type = msg_type
         self.update = update
 
     def __getitem__(self, item):
         return self.__getattribute__(item)
 
-    async def send(self, bot: ExtBot, chat: int) -> Message:
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
         ...
 
     @classmethod
@@ -61,7 +74,7 @@ class MessageFragmentText(MessageFragment):
         super().__init__(MessageType.TEXT, update)
         self.text = text
 
-    async def send(self, bot: ExtBot, chat: int) -> Message:
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
         return await bot.send_message(chat, text=self.text)
 
 
@@ -79,11 +92,187 @@ class MessageFragmentPhoto(MessageFragment):
         self.file = file
         self.caption = caption
 
-    async def send(self, bot: ExtBot, chat: int) -> Message:
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
         return await bot.send_photo(chat, photo=self.file, caption=self.caption)
 
 
+class MessageFragmentAnimation(MessageFragment):
+    file: str | Path | IO[bytes] | InputFile | bytes
+
+    def __init__(self, file: str | Path | IO[bytes] | InputFile | bytes, update: Update | None = None):
+        super().__init__(MessageType.ANIMATION, update)
+        self.file = file
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_animation(chat, animation=self.file)
+
+
+class MessageFragmentAudio(MessageFragment):
+    file: str | Path | IO[bytes] | InputFile | bytes
+
+    def __init__(self, file: str | Path | IO[bytes] | InputFile | bytes, update: Update | None = None):
+        super().__init__(MessageType.AUDIO, update)
+        self.file = file
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_audio(chat, audio=self.file)
+
+
+class MessageFragmentContact(MessageFragment):
+    contact: Contact
+
+    def __init__(self, contact: Contact, update: Update | None = None):
+        super().__init__(MessageType.CONTACT, update)
+        self.contact = contact
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_contact(
+            chat,
+            phone_number=self.contact.phone_number,
+            first_name=self.contact.first_name,
+            last_name=self.contact.last_name,
+        )
+
+
+class MessageFragmentDice(MessageFragment):
+    emoji: str
+
+    def __init__(self, emoji: str, update: Update | None = None):
+        super().__init__(MessageType.DICE, update)
+        self.emoji = emoji
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_dice(chat, emoji=self.emoji)
+
+
+class MessageFragmentDocument(MessageFragment):
+    file: str | Path | IO[bytes] | InputFile | bytes
+    caption: str | None
+
+    def __init__(
+        self,
+        file: str | Path | IO[bytes] | InputFile | bytes,
+        caption: str = None,
+        update: Update | None = None,
+    ):
+        super().__init__(MessageType.DOCUMENT, update)
+        self.file = file
+        self.caption = caption
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_document(chat, document=self.file, caption=self.caption)
+
+
+class MessageFragmentLocation(MessageFragment):
+    latitude: float
+    longitude: float
+
+    def __init__(self, latitude: float, longitude: float, update: Update | None = None):
+        super().__init__(MessageType.LOCATION, update)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_location(chat, latitude=self.latitude, longitude=self.longitude)
+
+
+class MessageFragmentVideo(MessageFragment):
+    file: str | Path | IO[bytes] | InputFile | bytes
+    caption: str | None
+
+    def __init__(
+        self,
+        file: str | Path | IO[bytes] | InputFile | bytes,
+        caption: str = None,
+        update: Update | None = None,
+    ):
+        super().__init__(MessageType.VIDEO, update)
+        self.file = file
+        self.caption = caption
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_video(chat, video=self.file, caption=self.caption)
+
+
+class MessageFragmentMediaGroup(MessageFragment):
+    media: list[MessageFragmentAudio | MessageFragmentDocument | MessageFragmentPhoto | MessageFragmentVideo]
+
+    def __init__(
+        self,
+        media: list[MessageFragmentAudio | MessageFragmentDocument | MessageFragmentPhoto | MessageFragmentVideo],
+        update: Update | None = None,
+    ):
+        super().__init__(_MISSING, update)
+        self.media = media
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> tuple[Message]:
+        media = []
+        cord = {
+            "photo": InputMediaPhoto,
+            "audio": InputMediaAudio,
+            "document": InputMediaDocument,
+            "video": InputMediaVideo,
+        }
+        for m in self.media:
+            media.append(cord[m.type](m.file))  # Captions should be discarded
+        return await bot.send_media_group(chat, media=media)
+
+
+class MessageFragmentSticker(MessageFragment):
+    file: str | Path | IO[bytes] | InputFile | bytes
+
+    def __init__(self, file: str | Path | IO[bytes] | InputFile | bytes, update: Update | None = None):
+        super().__init__(MessageType.STICKER, update)
+        self.file = file
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_sticker(chat, sticker=self.file)
+
+
+class MessageFragmentVenue(MessageFragment):
+    latitude: float
+    longitude: float
+    title: str
+    address: str
+
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        title: str,
+        address: str,
+        update: Update | None = None,
+    ):
+        super().__init__(MessageType.VENUE, update)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.title = title
+        self.address = address
+
+    async def send(self, bot: ExtBot, chat: int, /, reply_to: int = None) -> Message:
+        return await bot.send_venue(
+            chat,
+            latitude=self.latitude,
+            longitude=self.longitude,
+            title=self.title,
+            address=self.address,
+        )
+
+
 PRIORITIES: dict[type[MessageFragment], int] = {
-    MessageFragmentPhoto: 0,
+    # Others
+    MessageFragmentAnimation: 0,
+    MessageFragmentContact: 0,
+    MessageFragmentDice: 0,
+    MessageFragmentLocation: 0,
+    MessageFragmentSticker: 0,
+    MessageFragmentVenue: 0,
+    # Media Group
+    MessageFragmentPhoto: 1,
+    MessageFragmentAudio: 1,
+    MessageFragmentDocument: 1,
+    MessageFragmentVideo: 1,
+    MessageFragmentMediaGroup: 1,
+    # Text
     MessageFragmentText: float("inf"),  # Always at the end
 }
