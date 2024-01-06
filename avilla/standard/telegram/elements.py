447477b211be8e1ebb4bc3d81ef5cd66819b385a
nullqwertyuiop <null@member.fsf.org>
2024-01-07 01:35:37 +0800

    chore(standard/telegram): remove offset & length

--- avilla/standard/telegram/elements.py
+++ avilla/standard/telegram/elements.py
@@ -163,15 +163,14 @@ class Story(Element):
 
 @dataclass
 class Entity(Element):
-    offset: int
-    length: int
     text: str
 
     def __str__(self):
         return f"[${self.__class__.__name__}]"
 
     def __repr__(self):
-        return f"[${self.__class__.__name__}:offset={self.offset};length={self.length};text={self.text}]"
+        attrs = ";".join(f"{k}={v}" for k, v in self.__dict__.items())
+        return f"[${self.__class__.__name__}:{attrs}]"
 
 
 @dataclass
