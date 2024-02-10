39a2ff850b4b225c38d3e8a104545482100a06ec
nullqwertyuiop <null@member.fsf.org>
2024-02-10 20:41:47 +0800

    feat(standard/telegram): add webhook to MeCapability
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- avilla/standard/telegram/me/capability.py
+++ avilla/standard/telegram/me/capability.py
@@ -1,3 +1,6 @@
+from typing import Literal
+
+from avilla.core import Selector
 from avilla.core.ryanvk import TargetOverload
 from avilla.standard.telegram.constants import BotCommandScope
 from graia.ryanvk import Capability, Fn
@@ -5,23 +8,45 @@ from graia.ryanvk import Capability, Fn
 
 class MeCapability(Capability):
     @Fn.complex({TargetOverload(): ["target"]})
-    async def get_me(self) -> ...:
+    async def get_me(self, target: Selector) -> ...:
         ...
 
     @Fn.complex({TargetOverload(): ["target"]})
-    async def get_my_commands(self, *, scope: BotCommandScope = None, language_code: str = None) -> ...:
+    async def get_my_commands(
+        self, target: Selector, *, scope: BotCommandScope = None, language_code: str = None
+    ) -> ...:
         ...
 
     @Fn.complex({TargetOverload(): ["target"]})
-    async def get_my_description(self, *, language_code: str = None) -> str:
+    async def get_my_description(self, target: Selector, *, language_code: str = None) -> str:
         ...
 
     @Fn.complex({TargetOverload(): ["target"]})
-    async def get_my_short_description(self, *, language_code: str = None) -> str:
+    async def get_my_short_description(self, target: Selector, *, language_code: str = None) -> str:
         ...
 
     @Fn.complex({TargetOverload(): ["target"]})
-    async def get_my_name(self, *, language_code: str = None) -> str:
+    async def get_my_name(self, target: Selector, *, language_code: str = None) -> str:
         ...
 
-    # TODO set_*
+    @Fn.complex({TargetOverload(): ["target"]})
+    async def set_webhook(
+        self,
+        target: Selector,
+        *,
+        url: str,
+        max_connections: int = None,
+        allowed_updates: list[str] = None,
+        drop_pending_updates: bool = None,
+        ip_address: str = None,
+        certificate: ... = None,
+    ) -> Literal[True]:
+        pass
+
+    @Fn.complex({TargetOverload(): ["target"]})
+    async def delete_webhook(self, target: Selector, *, drop_pending_updates: bool = None) -> Literal[True]:
+        pass
+
+    @Fn.complex({TargetOverload(): ["target"]})
+    async def get_webhook_info(self, target: Selector) -> ...:
+        pass
