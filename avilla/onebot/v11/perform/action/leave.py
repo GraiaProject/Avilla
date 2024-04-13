605a0f75c5d2157af93e0e2dc92037618442debb
SerinaNya <34389622+SerinaNya@users.noreply.github.com>
2024-04-13 21:36:32 +0800

    fix(onebot.v11): add missing await (#160)
    * fix(onebot11): resolve #159
    
    * fix(onebot11): update actions/admin.py

--- avilla/onebot/v11/perform/action/leave.py
+++ avilla/onebot/v11/perform/action/leave.py
@@ -17,12 +17,12 @@ class OneBot11LeaveActionPerform((m := AccountCollector["OneBot11Protocol", "One
 
     @SceneCapability.leave.collect(m, target="land.group")
     async def leave_group(self, target: Selector):
-        result = self.account.connection.call("set_group_leave", {"group_id": int(target["group"])})
-        if result is None:
+        result = await self.account.connection.call("set_group_leave", {"group_id": int(target["group"])})
+        if result is not None:
             raise RuntimeError(f"Failed to leave group: {target}: {result}")
 
     @SceneCapability.disband.collect(m, target="land.group")
     async def disband_group(self, target: Selector):
-        result = self.account.connection.call("set_group_leave", {"group_id": int(target["group"]), "is_dismiss": True})
-        if result is None:
+        result = await self.account.connection.call("set_group_leave", {"group_id": int(target["group"]), "is_dismiss": True})
+        if result is not None:
             raise RuntimeError(f"Failed to disband group: {target}: {result}")
