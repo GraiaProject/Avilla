867230d5812dbe73e3e52bbe21b283ca8088a10d
nullqwertyuiop <null@member.fsf.org>
2024-05-10 18:11:18 +0800

    feat(onebot11): impl Nick, Summary, Avatar
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- avilla/onebot/v11/perform/action/scene.py
+++ avilla/onebot/v11/perform/action/scene.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 
 from avilla.core.ryanvk.collector.account import AccountCollector
 from avilla.core.selector import Selector
+from avilla.standard.core.profile import Avatar, Nick, Summary
 from avilla.standard.core.relation import SceneCapability
 
 if TYPE_CHECKING:
@@ -44,3 +45,45 @@ class OneBot11BanActionPerform((m := AccountCollector["OneBot11Protocol", "OneBo
         )
         if result is not None:
             raise RuntimeError(f"Failed to ban {target}: {result}")
+
+    @m.pull("land.group.member", Nick)
+    async def get_member_nick(self, target: Selector, route: ...) -> Nick:
+        result = await self.account.connection.call(
+            "get_group_member_info", {"group_id": int(target["group"]), "user_id": int(target["member"])}
+        )
+        if result is None:
+            raise RuntimeError(f"Failed to get member {target}")
+        return Nick(result.get("card", "") or result["nickname"], result["nickname"], result.get("title"))
+
+    @m.pull("land.friend", Nick)
+    @m.pull("land.stranger", Nick)
+    async def get_user_nick(self, target: Selector, route: ...) -> Nick:
+        result = await self.account.connection.call("get_stranger_info ", {"user_id": int(target["stranger"])})
+        if result is None:
+            raise RuntimeError(f"Failed to get stranger {target}")
+        return Nick(result["nickname"], result["nickname"], None)
+
+    @m.pull("land.group", Summary)
+    async def get_group_summary(self, target: Selector, route: ...) -> Summary:
+        result = await self.account.connection.call("get_group_info", {"group_id": int(target["group"])})
+        if result is None:
+            raise RuntimeError(f"Failed to get group {target}")
+        return Summary(result["group_name"], None)
+
+    @m.pull("land.friend", Summary)
+    @m.pull("land.stranger", Summary)
+    async def get_user_summary(self, target: Selector, route: ...) -> Summary:
+        result = await self.account.connection.call("get_stranger_info ", {"user_id": int(target["stranger"])})
+        if result is None:
+            raise RuntimeError(f"Failed to get stranger {target}")
+        return Summary(result["nickname"], None)
+
+    @m.pull("land.group.member", Avatar)
+    @m.pull("land.friend", Avatar)
+    @m.pull("land.stranger", Avatar)
+    async def get_user_avatar(self, target: Selector, route: ...) -> Avatar:
+        return Avatar(f"https://q2.qlogo.cn/headimg_dl?dst_uin={target.last_value}&spec=640")
+
+    @m.pull("land.group", Avatar)
+    async def get_group_avatar(self, target: Selector, route: ...) -> Avatar:
+        return Avatar(f"https://p.qlogo.cn/gh/{target.pattern['group']}/{target.pattern['group']}/")
